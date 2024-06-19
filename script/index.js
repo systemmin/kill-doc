fbcbbb5b1e6cdfda0773e9ef3ffe9f45704241e8
fmm <fangming78@sina.com>
2024-06-19 12:45:53 +0800

    发版3.1 PDF 横屏竖屏比例问题优化

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@
 // @name         【最强无套路脚本】你能看见多少我能下载多少&下载公开免费的PPT、PDF、DOC、TXT等文件
 // @namespace    http://tampermonkey.net/
 // @homepage	 https://github.com/systemmin/kill-doc
-// @version      3.0
+// @version      3.1
 // @description  百度|原创力|人人|360文库|豆丁|豆丁建筑|道客|MBA智库|得力|七彩学科|金锄头|爱问|蚂蚁|读根网|搜弘|微传网|淘豆网|GB|JJG|行业标准|轻竹办公|文泉书局|自然标准|飞书等公开免费文档下载
 // @author       Mr.Fang
 // @match        https://*.book118.com/*
@@ -1409,13 +1409,12 @@
 			} = await MF_CanvasToBase64(images[i]);
 			let fileName = i + ".png";
 			zipWriter.add(fileName, new zip.BlobReader(blob));
-			if (width > height) {
+			if (width > height) { // 横着
 				doc.addPage([width * pdf_ratio, height * pdf_ratio], 'l');
-				doc.addImage(images[i], 'JPEG', 0, 0, width * pdf_ratio, height * pdf_ratio, i, 'FAST')
-			} else {
+			} else { // 竖着
 				doc.addPage([width * pdf_ratio, height * pdf_ratio], 'p');
-				doc.addImage(images[i], 'JPEG', 0, 0, pdf_w, pdf_h, i, 'FAST')
 			}
+			doc.addImage(images[i], 'JPEG', 0, 0, width * pdf_ratio, height * pdf_ratio, i, 'FAST')
 			if (i === 1) {
 				doc.deletePage(1);
 			}
